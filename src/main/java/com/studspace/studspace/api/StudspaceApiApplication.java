58d154c99bf64b9801211f114bd26e684e95859c
darwin-s <discipler.093@gmail.com>
2024-09-01 17:22:35 +0300

    Add initial implementation

---
+++ src/main/java/com/studspace/studspace/api/StudspaceApiApplication.java
@@ -0,0 +1,18 @@
+// Copyright (c) 2024 StudSpace
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+package com.studspace.studspace.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudspaceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudspaceApiApplication.class, args);
+	}
+
+}
